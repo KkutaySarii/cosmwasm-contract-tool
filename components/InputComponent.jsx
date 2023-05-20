91d8041cf88ae2486e561d9b6a67eef0c43f3d86
Eyüp Bay <eyupbay7@gmail.com>
2023-05-20 19:58:38 +0300

    InputComponent updated

--- components/InputComponent.jsx
+++ components/InputComponent.jsx
@@ -12,7 +12,7 @@ function InputComponent() {
           <div className='text-center mb-4 font-semibold tracking-wider text-lg'>Step - {step}</div>
           <div className=' space-y-[17px] mb-[30px]'>
               <p>Enter your first input</p>
-              <input type="text" className=' w-full h-[60px] border border-white border-1 text-white bg-transparent p-2'/>
+              <textarea type="text" className=' w-full h-[100px] resize-none border border-white border-1 text-white bg-transparent p-2'/>
           </div>
           <div className=' flex items-center justify-center'>
               <button onClick={()=> setStep(step+1)} className='w-[240px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none'>Next</button>
@@ -28,7 +28,7 @@ function InputComponent() {
           <div className='text-center mb-4 font-semibold tracking-wider text-lg'>Step - {step}</div>
           <div className=' space-y-[17px] mb-[30px]'>
               <p>Enter your second input</p>
-              <input type="text" className=' w-full h-[60px] border border-white border-1 text-white bg-transparent p-2'/>
+              <textarea type="text" className=' w-full h-[100px] resize-none border border-white border-1 text-white bg-transparent p-2'/>
           </div>
           <div className='flex justify-between'>
               <button onClick={()=> setStep(step+1)} className='w-[240px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none'>Retry</button>
@@ -45,7 +45,7 @@ function InputComponent() {
           <div className='text-center mb-4 font-semibold tracking-wider text-lg'>Step - {step}</div>
           <div className=' space-y-[17px] mb-[30px]'>
               <p>Enter your third input</p>
-              <input type="text" className=' w-full h-[60px] border border-white border-1 text-white bg-transparent p-2'/>
+              <textarea type="text" className=' w-full h-[100px] resize-none border border-white border-1 text-white bg-transparent p-2'/>
           </div>
           <div className=' flex justify-between'>
               <button onClick={()=> setStep(step+1)} className='w-[240px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none'>Retry</button>
