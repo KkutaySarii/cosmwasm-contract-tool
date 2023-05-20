ebaa6c4f4d94e9a133047b843a57c670987fd2d3
Eyüp Bay <eyupbay7@gmail.com>
2023-05-20 20:36:51 +0300

    InputComponent updated

--- components/InputComponent.jsx
+++ components/InputComponent.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useState } from 'react'
-
-
+ 
 
 function InputComponent() {
   const [step,setStep] = useState(1)
+  const [prompt,setPrompt] = useState('')
   if (step==1){
     return (
       <div className='container mx-auto'>
@@ -12,10 +12,10 @@ function InputComponent() {
           <div className='text-center mb-4 font-semibold tracking-wider text-lg'>Step - {step}</div>
           <div className=' space-y-[17px] mb-[30px]'>
               <p>Enter your first input</p>
-              <textarea type="text" className=' w-full h-[100px] resize-none border border-white border-1 text-white bg-transparent p-2'/>
+              <textarea value={prompt} onChange={(e) => (setPrompt(e.target.value))} type="text" className=' w-full h-[100px] resize-none border border-white border-1 text-white bg-transparent p-2' required/>
           </div>
-          <div className=' flex items-center justify-center'>
-              <button onClick={()=> setStep(step+1)} className='w-[240px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none'>Next</button>
+          <div className='flex items-center justify-center'>
+              <button onClick={()=> {setStep(step+1);setPrompt("")}} className='w-[240px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none disabled:opacity-25 disabled:cursor-not-allowed' disabled={prompt===""}>Next</button>
           </div>
         </div>
       </div>
@@ -28,11 +28,11 @@ function InputComponent() {
           <div className='text-center mb-4 font-semibold tracking-wider text-lg'>Step - {step}</div>
           <div className=' space-y-[17px] mb-[30px]'>
               <p>Enter your second input</p>
-              <textarea type="text" className=' w-full h-[100px] resize-none border border-white border-1 text-white bg-transparent p-2'/>
+              <textarea value={prompt} onChange={(e) => (setPrompt(e.target.value))} type="text" className='w-full h-[100px] resize-none border border-white border-1 text-white bg-transparent p-2' required/>
           </div>
           <div className='flex justify-between'>
-              <button onClick={()=> setStep(step+1)} className='w-[240px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none'>Retry</button>
-              <button onClick={()=> setStep(step+1)} className='w-[550px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none'>Next</button>
+              <button onClick={()=> {setStep(1);setPrompt("")}} className='w-[240px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none'>Retry</button>
+              <button onClick={()=> {setStep(step+1);setPrompt("")}} className='w-[550px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none disabled:opacity-25 disabled:cursor-not-allowed' disabled={prompt===""}>Next</button>
           </div>
         </div>
       </div>
@@ -45,11 +45,11 @@ function InputComponent() {
           <div className='text-center mb-4 font-semibold tracking-wider text-lg'>Step - {step}</div>
           <div className=' space-y-[17px] mb-[30px]'>
               <p>Enter your third input</p>
-              <textarea type="text" className=' w-full h-[100px] resize-none border border-white border-1 text-white bg-transparent p-2'/>
+              <textarea value={prompt} onChange={(e)=>(setPrompt(e.target.value))} type="text" className=' w-full h-[100px] resize-none border border-white border-1 text-white bg-transparent p-2' required/>
           </div>
           <div className=' flex justify-between'>
-              <button onClick={()=> setStep(step+1)} className='w-[240px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none'>Retry</button>
-              <button onClick={()=> setStep(step+1)} className='w-[550px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none'>Summarize</button>
+              <button onClick={()=> {setStep(1);setPrompt("")}} className='w-[240px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none'>Retry</button>
+              <button onClick={()=> {setStep(step+1);setPrompt("")}} className='w-[550px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none disabled:opacity-25 disabled:cursor-not-allowed' disabled={prompt===""}>Summarize</button>
           </div>
         </div>
       </div>
