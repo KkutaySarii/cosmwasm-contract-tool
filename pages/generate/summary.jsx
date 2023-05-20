9cb226a3707f70e68de2b6a13992e02b9b6eb44b
Eyüp Bay <eyupbay7@gmail.com>
2023-05-21 00:56:48 +0300

    Buttons added

--- pages/generate/summary.jsx
+++ pages/generate/summary.jsx
@@ -9,20 +9,24 @@ function summary() {
         <div className='text-white text-poppins m-4 space-y-11'>
             <h1 className='text-center text-3xl font-semibold tracking-wider mb-4'>Summary</h1>
             <ul className="max-h-48 w-56 overflow-y-auto">
-  {query &&
-    query.map((data, index) => (
-      <li className='w-full' key={index} >
-        <div className="whitespace-pre-line">
-          {index + 1}. {data}
+                {query &&
+                    query.map((data, index) => (
+                    <li className='w-full' key={index} >
+                        <div className="whitespace-pre-line">
+                        {index + 1}. {data}
+                        </div>
+                        
+                    </li>
+                    ))}
+            </ul>
         </div>
-        
-      </li>
-    ))}
-</ul>
-        </div>
-        <div>
-            <button></button>
-            <button></button>
+        <div className='flex justify-between mx-12 gap-2 text-white text-poppins text-xl font-bold'>
+            <button
+              className="w-[150px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none"
+            >Back</button>
+            <button
+              className="w-[330px] h-[60px] bg-gradient-to-r from-[#4A1C85] to-[#35A4FD] bg-opacity-10 hover:scale-105 hover:shadow-lg duration-200 active:scale-95 active:shadow-none"
+            >Finish</button>
         </div>
     </div>
   )
